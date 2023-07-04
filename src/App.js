827838f0433d30b66e2650fd9a9113b82b168538
Sandarva <sandarva2003@gmail.com>
2023-07-05 00:15:04 +0530

    website

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Read from './components/Read';
 import Forms from './components/Forms';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Navbar title={"bachar iccha nai"}/>
-    <Forms/>
+    <Routes>
+    <Route path = "/" element =<Forms/>></Route>
+    <Route path ="/read" element =<Read/>></Route>
+    </Routes>
+    
    
     
     </>
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 export default function Forms(){
     const handleClick = () =>{
         setText  ("")
@@ -7,10 +7,28 @@ export default function Forms(){
     const handleChange = (event) =>{
         setText (event.target.value)
     }
+
+    const handleLower = () =>{
+        setText (text.toLowerCase())
+    }
+    const handleUpper = () =>{
+        setText (text.toUpperCase())
+    }
+    
+    const x = () =>{
+        setIndex (index+1)
+    }
+   
+
+    
+    const [index,setIndex] = useState(0)
     const [text,setText] = useState("enter text")
     return(
-        <><div><textarea value={text} id="pop" cols="30" rows="10" onClick={handleClick} onChange={handleChange}></textarea></div>
-        <div><button type="button" className="btn btn-primary">lower-case</button></div>  
+        <><div><textarea value={text} id="pop" cols="30" rows="10" onClick={handleClick} onChange={handleChange} ></textarea></div>
+        <div><button type="button" className="btn btn-primary"  onClick ={handleLower}>lower-case </button>
+        <button type ="button" className='btn btn-primary' onClick = {handleUpper}>upper-case</button>
+        </div>  
+       <button type="button" class="btn btn-warning" onClick =  {x}>Counter :{index}</button>
         </>
     )
 
